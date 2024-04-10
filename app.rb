a3ad8ba9fe6cc0d64ef3bfbe6623c99d95a7fc1c
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-10 18:14:01 +0000

    set up square root form and results

--- app.rb
+++ app.rb
@@ -23,4 +23,20 @@ get("/square/results") do
   @the_result = params.fetch("number_value").to_f * params.fetch("number_value").to_f
 
   erb(:square_results)
+
+end 
+get("/square_root/new") do 
+  "
+  <p>Enter a number
+  </p>"
+  erb(:square_root_form)
+ 
+end
+
+get("/square_root/results") do 
+
+  @the_num = params.fetch("number_value")
+  @the_result = Math.sqrt(params.fetch("number_value").to_f)
+
+  erb(:square_root_results)
 end 
