858741c7000989ba78f0ddfc18abe6b9719a682d
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-10 18:02:51 +0000

    fix errors in square form

--- app.rb
+++ app.rb
@@ -5,11 +5,22 @@ get("/") do
   "
   <p>Enter a number
   </p>"
-
   erb(:square_with_form)
 
 end
 
 get("/square/new") do 
- erb(:square_results)
+  "
+  <p>Enter a number
+  </p>"
+  erb(:square_with_form)
+ 
 end
+
+get("/square/results") do 
+
+  @the_num = params.fetch("number_value")
+  @the_result = params.fetch("number_value").to_f * params.fetch("number_value").to_f
+
+  erb(:square_results)
+end 
