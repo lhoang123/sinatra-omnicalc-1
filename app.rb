f1352efe263375a386471325d571faa104900144
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-10 23:39:28 +0000

    created payment form and result

--- app.rb
+++ app.rb
@@ -54,3 +54,21 @@ get("/random/results") do
   @the_result = rand(@the_min_num..@the_max_num)
   erb(:random_results)
 end 
+
+get("/payment/new") do 
+
+  erb(:payment_form)
+ 
+end
+
+get("/payment/results") do 
+  @apr_num = params.fetch("apr_input").to_f
+  @year_num = params.fetch("year_input").to_i
+  @principal_num = params.fetch("principal_input").to_f
+
+  @the_numerator = (@apr_num/1200 * @principal_num)
+  @the_denominator = (1-(1+@apr_num/100/12)**(-@year_num/12))
+  @the_result = (@the_numerator / @the_denominator).to_fs(:currency)
+
+  erb(:payment_results)
+end 
